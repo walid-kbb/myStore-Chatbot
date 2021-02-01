7aa9d0478021f86cce26745ae5cc08b7c02f9594
Walid <walid-kebbab@outlook.fr>
2021-02-01 17:27:14 +0100

    new commit

--- src/controllers/chatbotController.js
+++ src/controllers/chatbotController.js
@@ -72,12 +72,24 @@ let postWebHook=(req,res)=>{
 
 }
 
+// Handles post back events
+function handlePostback(sender_psid, received_postback) {
+    let response;
+    
+    // Get the payload for the postback
+    let payload = received_postback.payload;
+  
+    // Set the response based on the postback payload
+    if (payload === 'yes') {
+      response = { "text": "Thanks!" }
+    } else if (payload === 'no') {
+      response = { "text": "Oops, try sending another image." }
+    }
+    // Send the message to acknowledge the postback
+    callSendAPI(sender_psid, response);
+  }
+
 // Handles messages events
-function handleMessage(sender_psid, received_message) {
-
-}
-
-// Handles messaging_postbacks events
 function handleMessage(sender_psid, received_message) {
 
     let response;
@@ -89,7 +101,36 @@ function handleMessage(sender_psid, received_message) {
       response = {
         "text": `You sent the message: "${received_message.text}". Now send me an image!`
       }
-    }  
+    } else if (received_message.attachments) {
+  
+        // Gets the URL of the message attachment
+        let attachment_url = received_message.attachments[0].payload.url;
+        response = {
+            "attachment": {
+              "type": "template",
+              "payload": {
+                "template_type": "generic",
+                "elements": [{
+                  "title": "Is this the right picture?",
+                  "subtitle": "Tap a button to answer.",
+                  "image_url": attachment_url,
+                  "buttons": [
+                    {
+                      "type": "postback",
+                      "title": "Yes!",
+                      "payload": "yes",
+                    },
+                    {
+                      "type": "postback",
+                      "title": "No!",
+                      "payload": "no",
+                    }
+                  ],
+                }]
+            }
+            }
+         }
+      } 
     
     // Sends the response message
     callSendAPI(sender_psid, response);    
