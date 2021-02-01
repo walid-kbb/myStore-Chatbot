015ee0b4d099360927c89087fb55f8cbb9eb4d46
Walid <walid-kebbab@outlook.fr>
2021-02-01 16:44:06 +0100

    new commit

--- src/controllers/chatbotController.js
+++ src/controllers/chatbotController.js
@@ -42,10 +42,13 @@ let postWebHook=(req,res)=>{
       // Iterates over each entry - there may be multiple if batched
       body.entry.forEach(function(entry) {
   
-        // Gets the message. entry.messaging is an array, but 
-        // will only ever contain one message, so we get index 0
-        let webhook_event = entry.messaging[0];
-        console.log(webhook_event);
+          // Gets the body of the webhook event
+          let webhook_event = entry.messaging[0];
+          console.log(webhook_event);
+
+          // Get the sender PSID
+          let sender_psid = webhook_event.sender.id;
+          console.log('Sender PSID: ' + sender_psid);
       });
   
       // Returns a '200 OK' response to all requests
